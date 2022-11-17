6ead11a6fe932cd1f5c19325bc6b0df5645a5d10
alastair38 <alastair@alastaircox.com>
2022-11-17 15:25:34 +0000

    featured img to 'medium' size in featured-articles

--- components/content-featured-articles.php
+++ components/content-featured-articles.php
@@ -30,7 +30,7 @@ $articles = get_posts(array(
 					} else { echo get_the_permalink($item->ID); }?>
 					
 					">
-					<?php echo get_the_post_thumbnail($item->ID, 'portrait', array( 'class' => 'w-full' ));?>
+					<?php echo get_the_post_thumbnail($item->ID, 'medium', array( 'class' => 'w-full' ));?>
 					
 					 <span class="absolute bottom-2 left-2 right-2 bg-primary-default text-secondary px-1 py-1 lg:py-2" > <?php echo get_the_title($item->ID);?></span>
 					</a>
