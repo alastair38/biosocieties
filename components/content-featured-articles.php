38022e4bb50579b84cc8a5a5de57e4cc49ac59f6
alastair38 <alastair@alastaircox.com>
2023-01-12 14:30:58 +0000

    v0.88 focus/hover states content-featured-articles

--- components/content-featured-articles.php
+++ components/content-featured-articles.php
@@ -16,15 +16,15 @@ $articles = get_posts(array(
 	
 			<aside class="py-6 lg:py-12 px-6 lg:px-20">
 				<h2 class="w-11/12 mx-auto text-xl text-neutral-dark-100 font-black">Featured</h2>
-			<div class="grid gap-6 mt-6 w-11/12 pb-4 md:pb-0 scroll-px-4 overflow-x-auto mx-auto grid-flow-col auto-cols-[60%] md:auto-cols-auto md:grid-cols-5 overscroll-contain snap-x">
+			<div class="grid gap-6 mt-6 w-11/12 py-4 md:py-1 scroll-px-4 overflow-x-auto mx-auto grid-flow-col auto-cols-[60%] md:auto-cols-auto md:grid-cols-5 overscroll-contain snap-x">
 	
 		 <?php foreach($articles as $item):
 			$external_link = get_field('external_link', $item->ID);
 			?>
 			
 				
-				<div class="relative flex rounded-md snap-center overflow-hidden shadow-md">
-					<a class="w-full text-sm flex" href="
+				<div class="relative flex rounded-md snap-center shadow-md">
+					<a class="w-full h-full text-sm flex rounded-md overflow-hidden hover:ring-2 hover:ring-yellow-400 focus:ring-2 focus:ring-yellow-400" href="
 					<?php if($external_link) {
 					echo esc_url( $external_link );
 					} else { echo get_the_permalink($item->ID); }?>
@@ -32,7 +32,7 @@ $articles = get_posts(array(
 					">
 					<?php echo get_the_post_thumbnail($item->ID, 'large', array( 'class' => 'w-full' ));?>
 					
-					 <span class="absolute lg:min-h-[7rem] bottom-0 left-0 right-0 text-primary-default bg-neutral-dark-900 text-primary-default px-1 py-1 lg:py-2 lg:px-2 flex items-center font-serif" > <?php echo get_the_title($item->ID);?></span>
+					 <span class="absolute lg:min-h-[7rem] bottom-0 left-0 right-0 text-primary-default bg-neutral-dark-900 text-primary-default px-1 py-1 lg:py-2 lg:px-2 flex items-center font-serif rounded-b-md" > <?php echo get_the_title($item->ID);?></span>
 					</a>
 					
 				</div>
